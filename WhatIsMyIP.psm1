3443c781199aed064ae98679822eb15630524eea
Andy Svintsitsky <andy.svintsitsky@gmail.com>
2019-03-21 17:14:33 -0400

    Add files via upload

---
+++ WhatIsMyIP.psm1
@@ -0,0 +1,43 @@
+﻿
+<#
+.Synopsis
+   Get External IP address
+.DESCRIPTION
+   Get External IP address using free ipfy.org API.
+.EXAMPLE
+   PS C:\> Get-PublicIP
+0.0.0.1
+.EXAMPLE
+   PS C:\> whatismyip
+0.0.0.2
+#>
+function Get-PublicIP
+{
+    [CmdletBinding(SupportsShouldProcess=$true, 
+                  PositionalBinding=$false,
+                  ConfirmImpact='Medium')]
+    [Alias('whatismyip','Get-ExternalIP')]
+    Param
+    (
+
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        if ($pscmdlet.ShouldProcess("computer"))
+        {
+                $IPObj = Invoke-RestMethod -Uri 'https://api.ipify.org?format=json'
+                $IPObj.IP
+
+        }
+    }
+    End
+    {
+    }
+}
+
+
+
